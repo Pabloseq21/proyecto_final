5707a467d7a2c87dda3a3dd8a562c82f2880721a
Faryd Walteros <faryd.w.m@gmail.com>
2025-02-22 20:38:49 -0500

    Update constantes.py

--- pygames1.py/constantes.py
+++ pygames1.py/constantes.py
@@ -1,11 +1,14 @@
 import pygame
-# En este archivo se generan todas las constantes del proyecto para que quede mas organizado 
 
-# Fuentes
-front_inicio = pygame.font.SysFont("Minecraft", 30)
+pygame.init()  # Inicializa Pygame
+pygame.font.init()  # 🔹 Inicializa el módulo de fuentes
+
+# Ahora ya puedes cargar fuentes sin problemas
+front_inicio = pygame.font.Font("Font/Minecraft.ttf", 25)
+small_font = pygame.font.Font(None, 30)
 
 WIDTH = 800   # Ancho de la ventana
-HEIGHT = 600  # Alto de la ventana
+HEIGHT = 600 # Alto de la ventana
 
 # tamano ventana
 WIDTH_VENTANA = 200
@@ -19,4 +22,4 @@ MORADO = (138,43,226)
 TRANSPARENTE = (0,0,0,0)
 LIGHT_BLUE = (100, 100, 255)
 BLUE = (0, 0, 255)
-GRIS = (200,200,200)
+
